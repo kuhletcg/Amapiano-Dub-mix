08ed53748b7460fd6dcc0967212c53c7fdade427
kwanele-hub <kwanelemkhwanazi688@gmail.com>
2020-09-14 12:18:28 +0000

    add files(html and index.js)

---
+++ sound.js
@@ -0,0 +1,7 @@
+var startButton = document.getElementById('start');
+var sound = document.getElementById('sound');
+var bgm =document.getElementById('bgm');
+function play(){
+var  sound = new Audio('audio/https://thevinylfactory.com/wp-content/uploads/2016/09/harari.jpgg ');
+
+audio.play()};
